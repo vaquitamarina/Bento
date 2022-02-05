f653c7d282da6cc7f863ebab93ff981d9b50e24f
vaquitamarina <87457016+vaquitamarina@users.noreply.github.com>
2022-02-05 09:34:04 -0500

    Update config.js

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ const CONFIG = {
 
   // Autochange by hour options (24hrs format, string must be in: hh:mm)
   changeThemeByHour: true,
-  hourDarkThemeActive: '18:30',
+  hourDarkThemeActive: '07:00',
   hourDarkThemeInactive: '07:00',
 
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
